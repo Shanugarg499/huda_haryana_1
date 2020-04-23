10b775f0ac496d8b9e3a0a7dbadbae6544710365
Anubhav <transformer12900@gmail.com>
2020-04-23 19:31:28 +0530

    Notes added

---
+++ app/src/main/java/com/example/huda_haryana/mybusiness/NewNote.kt
@@ -0,0 +1,13 @@
+package com.example.huda_haryana.mybusiness
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.huda_haryana.R
+
+class NewNote : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_new_note)
+    }
+}
