e0c4e248fd3ebb0d9d82c2c35ec507489262f91e
Shanu Garg <shanugarg499@gmail.com>
2020-05-05 22:24:16 +0530

    Facebook added

--- app/src/main/java/com/example/huda_haryana/very_main.java
+++ app/src/main/java/com/example/huda_haryana/very_main.java
@@ -15,6 +15,15 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.facebook.AccessToken;
+import com.facebook.AccessTokenTracker;
+import com.facebook.CallbackManager;
+import com.facebook.FacebookCallback;
+import com.facebook.FacebookException;
+import com.facebook.GraphRequest;
+import com.facebook.GraphResponse;
+import com.facebook.login.LoginResult;
+import com.facebook.login.widget.LoginButton;
 import com.google.android.gms.auth.api.signin.GoogleSignIn;
 import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
 import com.google.android.gms.auth.api.signin.GoogleSignInClient;
@@ -31,6 +40,11 @@ import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.auth.GoogleAuthProvider;
 import com.squareup.picasso.Picasso;
 
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.Arrays;
+
 public class very_main extends AppCompatActivity {
 
     ProgressBar progressBar;
@@ -38,6 +52,8 @@ public class very_main extends AppCompatActivity {
     private GoogleSignInClient mGoogleSignInClient;
     private FirebaseAuth mAuth;
     private EditText Email, Password;
+    private LoginButton loginButton;
+    private CallbackManager callbackManager;
     private int RC_SIGN_IN = 1;
 
     @Override
@@ -48,9 +64,30 @@ public class very_main extends AppCompatActivity {
         Password = findViewById(R.id.password);
         mAuth = FirebaseAuth.getInstance();
         progressBar = findViewById(R.id.progressBar2);
+        loginButton = findViewById(R.id.fb_login);
         mAuth = FirebaseAuth.getInstance();
+        callbackManager = CallbackManager.Factory.create();
+        loginButton.setPermissions(Arrays.asList("email", "public_profile"));
 
+//        checkloginstatus();
 
+        loginButton.registerCallback(callbackManager, new FacebookCallback<LoginResult>() {
+            @Override
+            public void onSuccess(LoginResult loginResult) {
+                startActivity(new Intent(getApplicationContext(), MainActivity.class));
+                Toast.makeText(getApplicationContext(), "Successful", Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onCancel() {
+
+            }
+
+            @Override
+            public void onError(FacebookException error) {
+
+            }
+        });
 
 
     findViewById(R.id.login_txt).setOnClickListener(new View.OnClickListener() {
@@ -140,5 +177,58 @@ public class very_main extends AppCompatActivity {
 //        }
 //    }
 
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        callbackManager.onActivityResult(requestCode, resultCode, data);
+        super.onActivityResult(requestCode, resultCode, data);
+    }
+
+    AccessTokenTracker accessTokenTracker = new AccessTokenTracker() {
+        @Override
+        protected void onCurrentAccessTokenChanged(AccessToken oldAccessToken, AccessToken currentAccessToken) {
+            if(currentAccessToken==null){
+                Toast.makeText(getApplicationContext(), "User logged out", Toast.LENGTH_SHORT).show();
+            }
+            else{
+                loaduserProfile(currentAccessToken);
+            }
+        }
+    };
+
+    private void loaduserProfile(AccessToken newAccessToken){
+
+        GraphRequest request = GraphRequest.newMeRequest(newAccessToken, new GraphRequest.GraphJSONObjectCallback() {
+            @Override
+            public void onCompleted(JSONObject object, GraphResponse response) {
+                try {
+                    String first_name = object.getString("first_name");
+                    String last_name = object.getString("last_name");
+                    String email = object.getString("email");
+                    String id = object.getString("id");
+
+                     Toast.makeText(getApplicationContext(), first_name + " " +last_name + " logged in successfully", Toast.LENGTH_SHORT).show();
+                     startActivity(new Intent(getApplicationContext(), MainActivity.class));
+
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+
+            }
+        });
+
+        Bundle parameters = new Bundle();
+        parameters.putString("fields","first_name, last_name, email, id");
+        request.setParameters(parameters);
+        request.executeAsync();
+
+    }
+
+//    private void checkloginstatus(){
+//        if(AccessToken.getCurrentAccessToken() != null){
+//            Toast.makeText(getApplicationContext(), " You already logged in ", Toast.LENGTH_SHORT).show();
+//            startActivity(new Intent(getApplicationContext(), MainActivity.class));
+//        }
+//    }
 
 }
