659019f066e3cb6567b6e65dbe1cc7af3f6b2ef7
Anubhav <transformer12900@gmail.com>
2020-05-30 20:37:26 +0530

    Added extended types

--- app/src/main/java/com/example/huda_haryana/Lead/LeadOptions.kt
+++ app/src/main/java/com/example/huda_haryana/Lead/LeadOptions.kt
@@ -1,10 +1,12 @@
 package com.example.huda_haryana.Lead
 
+import android.app.AlertDialog
 import android.app.Dialog
+import android.content.DialogInterface
 import android.content.Intent
+import android.content.res.TypedArray
 import android.net.Uri
 import android.os.Bundle
-import android.view.View
 import android.widget.*
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
@@ -21,7 +23,12 @@ import com.google.firebase.database.ValueEventListener
 
 class LeadOptions : AppCompatActivity() {
     lateinit var binding: ActivityLeadOptionsBinding
+    lateinit var check: String
     var typeData: TypeData? = null
+    var type: String = ""
+    var subtype: String = ""
+    var second_type: String = ""
+    var second_sub: String = ""
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = DataBindingUtil.setContentView(this, R.layout.activity_lead_options)
@@ -125,43 +132,180 @@ class LeadOptions : AppCompatActivity() {
 
         })
         val typeref = FirebaseDatabase.getInstance().getReference("User").child(acct.id!!).child("Leads").child(id).child("Type")
-        val items = arrayListOf<String>("ADD PROPERTY TYPE", "BUY", "SELL", "RENT")
-        val adapter = ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, items)
-        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
-        binding.leadoptionSpinner.adapter = adapter
-        typeref.addValueEventListener(object : ValueEventListener {
-            override fun onCancelled(p0: DatabaseError) {
 
-            }
+        val alertDialog = AlertDialog.Builder(this)
+        val alert2 = AlertDialog.Builder(this)
+        val alert3 = AlertDialog.Builder(this)
+        val items = arrayListOf<String>("RESIDENTIAL", "COMMERCIAL")
+        val adapter = ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, items)
+        val items2 = arrayListOf<String>("BUY", "SELL", "RENT")
+        val adapter2 = ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, items2)
+        val item3 = arrayListOf<String>("SHOPS", "SCO", "SCF", "Other")
+        val adapter3 = ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, item3)
+        val alert5 = AlertDialog.Builder(this)
+        val items5 = arrayListOf<String>("60sq. Yards", "100sq. Yards", "160sq. Yards", "250sq. Yards", "500sq. yards", "1000sq. yards")
+        val adapter5 = ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, items5)
+        alert5.setAdapter(adapter5, object : DialogInterface.OnClickListener {
+            override fun onClick(dialog: DialogInterface?, which: Int) {
+                when (which) {
+                    0 -> {
+                        second_sub = "60sq. Yards"
+                    }
+                    1 -> {
+                        second_sub = "100sq. Yards"
+                    }
+                    2 -> {
+                        second_sub = "160sq. Yards"
+                    }
+                    3 -> {
+                        second_sub = "250sq. Yards"
+                    }
+                    4 -> {
+                        second_sub = "500sq. Yards"
+                    }
+                    5 -> {
+                        second_sub = "1000sq. Yards"
+                    }
 
-            override fun onDataChange(p0: DataSnapshot) {
-                if (p0.exists()) {
-                    typeData = p0.getValue(TypeData::class.java)!!
-                    binding.leadoptionSpinner.setSelection(typeData!!.pos.toInt())
                 }
+                typeref.setValue(TypeData(type,second_type,subtype,second_sub))
+                dialog?.dismiss()
             }
 
         })
-
-        binding.leadoptionSpinner.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
-            override fun onNothingSelected(parent: AdapterView<*>?) {
-
-            }
-
-            override fun onItemSelected(parent: AdapterView<*>?, view: View?, position: Int, id: Long) {
-                when (position) {
+        val alert6 = AlertDialog.Builder(this)
+        val items6 = arrayListOf<String>("Studio Apartment", "1BHK", "2BHK", "2+1 BHK", "3BHK", "3+1 BHK", "4BHK", "PentHouse")
+        val adapter6 = ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, items6)
+        alert6.setAdapter(adapter6, object : DialogInterface.OnClickListener {
+            override fun onClick(dialog: DialogInterface?, which: Int) {
+                when (which) {
+                    0 -> {
+                        second_sub = "Studio Apartment"
+                    }
                     1 -> {
-                        typeref.setValue(TypeData("BUY", "1"))
+                        second_sub = "1BHK"
                     }
                     2 -> {
-                        typeref.setValue(TypeData("SELL", "2"))
+                        second_sub = "2BHK"
+
                     }
                     3 -> {
-                        typeref.setValue(TypeData("RENT", "3"))
+                        second_sub = "2+1 BHK"
+                    }
+                    4 -> {
+                        second_sub = "3BHK"
+                    }
+                    5 -> {
+                        second_sub = "3+1 BHK"
+                    }
+                    6 -> {
+                        second_sub = "4BHK"
+                    }
+                    7 -> {
+                        second_sub = "PentHouse"
                     }
                 }
+                typeref.setValue(TypeData(type,second_type,subtype,second_sub))
+                dialog?.dismiss()
             }
 
+        })
+        val alert4 = AlertDialog.Builder(this)
+        val items4 = arrayListOf<String>("PLOT", "VILLA", "KOTHI", "FLAT", "Other")
+        val adapter4 = ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, items4)
+        alert4.setAdapter(adapter4, object : DialogInterface.OnClickListener {
+            override fun onClick(dialog: DialogInterface?, which: Int) {
+                when (which) {
+                    0 -> {
+                        subtype = "PLOT"
+                        alert5.show()
+                        dialog?.dismiss()
+                        return
+                    }
+                    1 -> {
+                        subtype = "VILLA"
+                    }
+                    2 -> {
+                        subtype = "KOTHI"
+                    }
+                    3 -> {
+                        subtype = "FLAT"
+                        alert6.show()
+                        dialog?.dismiss()
+                        return
+                    }
+                    4 -> {
+                        subtype = "Other"
+                    }
+                }
+                typeref.setValue(TypeData(type,second_type,subtype))
+                dialog?.dismiss()
+            }
+
+        })
+        alert2.setAdapter(adapter2, object : DialogInterface.OnClickListener {
+            override fun onClick(dialog: DialogInterface?, which: Int) {
+                when (which) {
+                    0 -> {
+                        second_type = "BUY"
+                    }
+                    1 -> {
+                        second_type = "SELL"
+                    }
+                    2 -> {
+                        second_type = "RENT"
+                    }
+                }
+                if (type == "COMMERCIAL")
+                    alert3.show()
+                if (type == "RESIDENTIAL")
+                    alert4.show()
+                dialog?.dismiss()
+            }
+
+        })
+        alert3.setAdapter(adapter3, object : DialogInterface.OnClickListener {
+            override fun onClick(dialog: DialogInterface?, which: Int) {
+                when (which) {
+                    0 -> {
+
+                        subtype = "SHOPS"
+                    }
+                    1 -> {
+                        subtype = "SCO"
+                    }
+                    2 -> {
+                        subtype = "SCF"
+                    }
+                    3 -> {
+                        subtype = "Other"
+                    }
+                }
+                typeref.setValue(TypeData(type,second_type,subtype))
+                dialog?.dismiss()
+
+            }
+
+        })
+        binding.leadoptionAddtype.setOnClickListener {
+            alertDialog.show()
         }
+        alertDialog.setAdapter(adapter, object : DialogInterface.OnClickListener {
+            override fun onClick(dialog: DialogInterface?, which: Int) {
+                alert2.show()
+                when (which) {
+                    0 -> {
+
+                        type = "RESIDENTIAL"
+                    }
+                    1 -> {
+
+                        type = "COMMERCIAL"
+                    }
+                }
+                dialog?.dismiss()
+            }
+
+        })
     }
 }
--- app/src/main/java/com/example/huda_haryana/Lead/TypeData.kt
+++ app/src/main/java/com/example/huda_haryana/Lead/TypeData.kt
@@ -1,3 +1,3 @@
 package com.example.huda_haryana.Lead
 
-data class TypeData(val type:String="",val pos:String="")
+data class TypeData(val type:String="",val second_type:String="",val subtype:String="",val second_sub:String="")
