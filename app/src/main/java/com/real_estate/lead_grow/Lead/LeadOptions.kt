c885540ceab1cdc89a6d50ff292f52d22cb3ef5a
Anubhav <transformer12900@gmail.com>
2020-06-02 00:09:58 +0530

    Package changed

--- app/src/main/java/com/real_estate/lead_grow/Lead/LeadOptions.kt
+++ app/src/main/java/com/real_estate/lead_grow/Lead/LeadOptions.kt
@@ -293,7 +293,6 @@ class LeadOptions : AppCompatActivity() {
                 alert2.show()
                 when (which) {
                     0 -> {
-
                         type = "RESIDENTIAL"
                     }
                     1 -> {
